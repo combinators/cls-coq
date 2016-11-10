531640d45057935a108edca00348bcae5b7db871
JanBessai <jan.bessai@tu-dortmund.de>
2016-11-10 11:50:20 +0100

    Constant embeddings fixed

--- SortEmbeddings.v
+++ SortEmbeddings.v
@@ -5,39 +5,187 @@ Require Import SigmaAlgebra.
 Require Import CombinatoryLogic.
 Require Import IntersectionTypes.
 Require Import CombinatoryLogicAlgebra.
+Require Import Cantor.
 
-Module Type ConstantSortCLSignature <: CompatibleCLSignature.
-  Include SignatureSpec with Definition Var := EmptySet.
-  Definition ConstructorSymbol := Sort EmptySet.
+Module Type FiniteCountableSignatureSpec <: SignatureSpec.
+  Include SignatureSpec.
+  Declare Instance ClosedSortsCountable: Countable (Sort EmptySet).
+  Declare Instance Vars_finite: Finite Var.
+  Parameter OpenSortsInhabited: Sort Var.
+End FiniteCountableSignatureSpec.
+
+Module Type SortConstructorCLSignature(SigSpec: FiniteCountableSignatureSpec) <: CompatibleCLSignature.
+  Definition Sort: Set -> Set := SigSpec.Sort.
+  Definition Var: Set := SigSpec.Var.
+  Definition Operation: Set := SigSpec.Operation.
+  Definition WellFormed: (Var -> Sort EmptySet) -> Prop := SigSpec.WellFormed.
+  Instance SigSpec: SignatureSpecification Sort Var Operation := SigSpec.SigSpec.
+  
+  Definition ConstructorSymbol := (unit + Sort EmptySet)%type.
   Definition VariableSymbol := Var.
   Definition CombinatorSymbol := Operation.
   Definition VariableSymbol_eq_dec := Var_eq_dec.
-  Instance Symbols: ConstructorSpecification ConstructorSymbol :=
-    {| constructorArity := fun _ => 0;
-       ConstructorTaxonomy := subsorts;
-       CTPreorder := subsorts_pre;
-       ConstructorSymbol_eq_dec := Sort_eq_dec;
-       ConstructorTaxonomy_dec := subsorts_dec |}.
+  Module ConstructorDefs.
+    Definition ConstructorTaxonomy: ConstructorSymbol -> ConstructorSymbol -> Prop :=
+      fun c1 c2 =>
+        match c1 with
+        | inr c1 =>
+          match c2 with
+          | inr c2 => subsorts c1 c2
+          | _ => False
+          end
+        | inl tt =>
+          match c2 with
+          | inl tt => True
+          | inr _ => False
+          end
+        end.
+    Lemma ConstructorTaxonomy_reflexive: Reflexive ConstructorTaxonomy.
+    Proof.
+      intro x.
+      destruct x as [ [ ] | s ].
+      - simpl; trivial.
+      - simpl.
+        generalize (subsorts_pre).
+        intro presub.
+        reflexivity.
+    Qed.
 
-  Parameter SortsInhabited: Sort EmptySet.
+    Lemma ConstructorTaxonomy_transitive: Transitive ConstructorTaxonomy.
+    Proof.
+      intros x y z.
+      destruct x as [ [] | x ]; destruct y as [ [] | y ]; destruct z as [ [] | z ];
+        intros xy yz;
+        try solve [ contradiction | trivial ].
+      generalize (subsorts_pre); intro presub.
+      simpl.
+      transitivity y; assumption.
+    Qed.
+
+    Lemma ConstructorTaxonomy_eq_dec: forall (c1 c2: ConstructorSymbol), { c1 = c2 } + { c1 <> c2 }.
+    Proof.
+      intros c1 c2.
+      destruct c1 as [ [] | c1 ]; destruct c2 as [ [] | c2 ];
+        try solve [ left; reflexivity | right; intro devil; inversion devil ].
+      generalize (SigSpec).
+      intro sigspec.
+      destruct (Sort_eq_dec c1 c2);
+        [ left; apply f_equal; assumption
+        | right; intro devil; inversion devil; contradiction ].
+    Qed.
+
+    Lemma ConstructorTaxonomy_dec: forall c1 c2, { ConstructorTaxonomy c1 c2 } + { ConstructorTaxonomy c1 c2 -> False }.
+    Proof.
+      intros c1 c2.
+      destruct c1 as [ [] | ]; destruct c2 as [ [] | ];
+        try solve [ left; reflexivity | right; intro; assumption ].
+      apply subsorts_dec.
+    Qed.
+
+    Definition constructorSymbolToNat (c: ConstructorSymbol): nat :=
+      match c with
+      | inl tt => cantor_sum (inl 0)
+      | inr c => cantor_sum (inr (toNat c))
+      end.
+    Definition natToConstructorSymbol (n: nat): ConstructorSymbol :=
+      match cantor_sum_inv n with
+      | inl _ => inl tt
+      | inr n => inr (fromNat n)
+      end.
+    Lemma fromToConstructorSymbol_id: forall c, natToConstructorSymbol (constructorSymbolToNat c) = c.
+    Proof.
+      intro c.
+      destruct c as [ [] | c ].
+      - reflexivity.
+      - unfold constructorSymbolToNat.
+        unfold natToConstructorSymbol.
+        rewrite cantor_sum_inj.
+        rewrite fromTo_id.
+        reflexivity.
+    Qed.
+  End ConstructorDefs.
+    
+  Definition Symbols: ConstructorSpecification ConstructorSymbol :=
+    {| constructorArity := fun c => match c with | inl _ => 1 | inr _ => 0 end;
+       ConstructorTaxonomy := ConstructorDefs.ConstructorTaxonomy;        
+       CTPreorder :=
+         {| PreOrder_Reflexive := ConstructorDefs.ConstructorTaxonomy_reflexive;
+            PreOrder_Transitive := ConstructorDefs.ConstructorTaxonomy_transitive |};
+       ConstructorSymbol_eq_dec := ConstructorDefs.ConstructorTaxonomy_eq_dec;
+       ConstructorTaxonomy_dec := ConstructorDefs.ConstructorTaxonomy_dec |}.
+  Instance ConstructorSymbol_countable: Countable ConstructorSymbol :=
+    {| toNat := ConstructorDefs.constructorSymbolToNat;
+       fromNat := ConstructorDefs.natToConstructorSymbol;
+       fromTo_id := ConstructorDefs.fromToConstructorSymbol_id |}.
+  Instance Variables_finite: Finite VariableSymbol := SigSpec.Vars_finite.
+  Definition ClosedSortsInhabited: Sort EmptySet := fromNat 0.
+  Definition OpenSortsInhabited: Sort Var := SigSpec.OpenSortsInhabited.
+  Definition BlackBox : ConstructorSymbol := inl tt.
+  Lemma BlackBoxArity: @constructorArity _ Symbols BlackBox = 1.
+  Proof.
+    reflexivity.
+  Qed.
+End SortConstructorCLSignature.
+
+Module Type CountableClosedSortSignatureSpec.
+  Include SignatureSpec with Definition Var := EmptySet.
+  Declare Instance ClosedSortsCountable: Countable (Sort EmptySet).
   Parameter applySubst_eq: forall S s, applySubst S s = s.
-End ConstantSortCLSignature.
+End CountableClosedSortSignatureSpec.
+
+Module Type FiniteCountableFromCountableClosedSortSignatureSpec
+       (SigSpec: CountableClosedSortSignatureSpec) <: FiniteCountableSignatureSpec.
+  Definition Sort: Set -> Set := SigSpec.Sort.
+  Definition Var: Set := SigSpec.Var.
+  Definition Operation: Set := SigSpec.Operation.
+  Definition WellFormed: (Var -> Sort EmptySet) -> Prop := SigSpec.WellFormed.
+  Instance SigSpec: SignatureSpecification Sort Var Operation := SigSpec.SigSpec.
+  Instance ClosedSortsCountable: Countable (Sort EmptySet) := SigSpec.ClosedSortsCountable.
+  Instance Vars_finite: Finite Var :=
+    {| cardinality := 0;
+       toFin := fun devil => False_rect _ devil;
+       fromFin := fun devil =>
+                    match devil in Fin.t n return 0 = n -> EmptySet with
+                    | Fin.F1 =>
+                      fun devil' =>
+                        match devil' in _ = n return match n with | 0 => True | _ => False end with
+                        | eq_refl => I
+                        end  
+                    | Fin.FS _ =>
+                      fun devil' =>
+                        match devil' in _ = n return match n with | 0 => True | _ => False end with
+                        | eq_refl => I
+                        end
+                    end eq_refl;
+       fromToFin_id := fun devil => False_rect _ devil |}.
+  Definition OpenSortsInhabited: Sort Var := fromNat 0.
+End FiniteCountableFromCountableClosedSortSignatureSpec.
+
+Module Type MakeClosedSortCLSignature(SigSpec: CountableClosedSortSignatureSpec).
+  Declare Module SigSpec' : FiniteCountableFromCountableClosedSortSignatureSpec(SigSpec).
+  Declare Module Signature : SortConstructorCLSignature(SigSpec').
+End MakeClosedSortCLSignature.
+
 
 Module Type ProperConstantEmbedding
-       (Import SigSpec: ConstantSortCLSignature)
-       (Import Logic: CombinatoryLogicSignature(SigSpec)) <: WithProperEmbedding(SigSpec)(Logic).
-  Module Embedding : SortEmbedding(SigSpec)(Logic).
-    Include SortEmbedding(SigSpec)(Logic).
+       (Import SigSpec: CountableClosedSortSignatureSpec)
+       (Import MakeSignature: MakeClosedSortCLSignature(SigSpec))
+       (Import Types: IntersectionTypes(MakeSignature.Signature))
+<: WithProperEmbedding(MakeSignature.Signature)(Types).
+  
+  Module Embedding : SortEmbedding(Signature)(Types).
+    Include SortEmbedding(Signature)(Types).
   End Embedding.
 
   Import Embedding.
 
   Module Proper.
-    Definition embedS (s: Sort EmptySet) := ConstScheme s (nil _).
+    Import Signature.
+    Definition embedS (s: Sort EmptySet) := ConstScheme (inr s) (nil _).
     Definition unembedS (ts: TypeScheme (VariableSymbol := EmptySet)): Sort EmptySet :=
       match ts with
-      | Skeleton (PT_Const C _) => C
-      | _ => SortsInhabited
+      | Skeleton (PT_Const (inr C) _) => C
+      | _ => OpenSortsInhabited
       end.
 
     Instance ClosedEmbedding : Embedding EmptySet :=
@@ -54,7 +202,7 @@ Module Type ProperConstantEmbedding
     Lemma embed_respectful: forall s s', subsorts s s' -> freeze (embed s) <= freeze (embed s').
     Proof.
       intros s s' s_le.
-      apply (ST_Ax s s' eq_refl); [ assumption | apply Forall2_nil ].
+      apply (ST_Ax (inr s) (inr s') eq_refl); [ assumption | apply Forall2_nil ].
     Qed.
 
     Lemma unembed_respectful: forall (sigma tau: IntersectionType),
